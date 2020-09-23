f7fd87539c8e2181975509f450a3f60901da6db8
fcsouza <fcsouza18@hotmail.com>
2020-09-23 20:58:21 -0300

    include jest

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  bail: true,
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["src/controllers/UserController.ts**"],
+  coverageDirectory: "test/coverage",
+  coveragePathIgnorePatterns: [
+    "ormconfig.js",
+    "/test/",
+    "/node_modules/",
+    "/src/middlewares",
+    "/src/models",
+    "/src/helpers",
+    "/src/database/migrations",
+    "/src/helpers/",
+    "/build/",
+    "/routes",
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  globals: {
+    "ts-jest": {
+      diagnostics: false,
+      isolatedModules: true,
+    },
+  },
+  modulePaths: ["<rootDir>/src/"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/test/**/*.test.ts?(x)"],
+};
